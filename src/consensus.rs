90c90c50cc5f6d261ece393601f2291f221d662c
ackintosh <sora.akatsuki@gmail.com>
2022-09-12 15:44:40 +0900

    Cache consensus document

--- src/consensus.rs
+++ src/consensus.rs
@@ -1,8 +1,43 @@
-use chrono::{DateTime, NaiveDateTime, ParseResult, Utc};
+use chrono::{DateTime, NaiveDateTime, Utc};
+
+const CACHE_KEY_BODY: &str = "consensus_document_body";
+const CACHE_KEY_VALID_UNTIL: &str = "consensus_document_valid_until";
+
+fn cache_dir() -> String {
+    format!("{}/.gants", dirs::home_dir().unwrap().display())
+}
+
+pub(crate) async fn cache_consensus_document(consensus: &String, valid_until: &DateTime<Utc>) {
+    cacache::write(cache_dir(), CACHE_KEY_BODY, consensus)
+        .await
+        .unwrap();
+    cacache::write(cache_dir(), CACHE_KEY_VALID_UNTIL, valid_until.to_rfc3339())
+        .await
+        .unwrap();
+}
+
+pub(crate) async fn get_consensus_document_from_cache(now: &DateTime<Utc>) -> Option<String> {
+    let valid_until = match cacache::read(cache_dir(), CACHE_KEY_VALID_UNTIL).await {
+        Ok(s) => {
+            let valid_until_string = String::from_utf8(s).unwrap();
+            DateTime::parse_from_rfc3339(&valid_until_string).unwrap()
+        }
+        Err(e) => {
+            println!("{:?}", e);
+            return None;
+        }
+    };
+
+    if &valid_until < now {
+        return None;
+    }
+
+    Some(String::from_utf8(cacache::read(cache_dir(), CACHE_KEY_BODY).await.unwrap()).unwrap())
+}
 
 // https://github.com/torproject/torspec/blob/main/dir-spec.txt
 // 3.4.1. Vote and consensus status document formats
-pub(crate) fn parse_consensus_document(consensus: String) -> Result<Consensus, ParseError> {
+pub(crate) fn parse_consensus_document(consensus: &String) -> Result<Consensus, ParseError> {
     let mut valid_after = None;
     let mut valid_until = None;
 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod consensus;
 
-use crate::consensus::parse_consensus_document;
-use chrono::{NaiveDateTime, Utc};
+use crate::consensus::{
+    cache_consensus_document, get_consensus_document_from_cache, parse_consensus_document,
+};
+use chrono::Utc;
 use std::net::Ipv4Addr;
 
 // *** Specs ***
@@ -18,17 +20,24 @@ async fn main() {
     // The consensus document is compressed using deflate algorithm.
     let client = reqwest::Client::builder().deflate(true).build().unwrap();
 
-    // TODO: Select directory authority randomly.
-    let da = directory_authorities().pop().unwrap();
-    println!("{}", da.consensus_url());
-    // TODO: error handling
-    let res = client.get(da.consensus_url()).send().await.unwrap();
-    // TODO: error handling
-    let body = res.text().await.unwrap();
+    let body = if let Some(consensus_document) = get_consensus_document_from_cache(&now).await {
+        println!("Using cached consensus document.");
+        consensus_document
+    } else {
+        // TODO: Select directory authority randomly.
+        let da = directory_authorities().pop().unwrap();
+        println!("Downloading consensus document from {}", da.consensus_url());
+        // TODO: error handling
+        let res = client.get(da.consensus_url()).send().await.unwrap();
+        // TODO: error handling
+        res.text().await.unwrap()
+    };
 
     // TODO: error handling
-    let consensus = parse_consensus_document(body).unwrap();
+    let consensus = parse_consensus_document(&body).unwrap();
     assert!(consensus.valid_after <= now && now <= consensus.valid_until);
+
+    cache_consensus_document(&body, &consensus.valid_until).await;
     println!("{:?}", consensus);
 }
 
@@ -38,16 +47,6 @@ fn directory_authorities() -> Vec<DirectoryAuthority> {
         DirectoryAuthority::new("maatuska".into(), Ipv4Addr::new(171, 25, 193, 9), 443, 80),
         // DirectoryAuthority::new("moria1".into(), Ipv4Addr::new(128, 31, 0, 34), 9131, 9101),
     ]
-}
-
-fn cache_consensus_document(consensus: &String) {
-    for line in consensus.lines() {
-        if line.starts_with("valid-until") {
-            let vu = line.split_whitespace().collect::<Vec<_>>();
-            assert_eq!(2, vu.len());
-            let dt = NaiveDateTime::parse_from_str(vu[1], "%Y-%m-%d %H:%M:%S");
-        }
-    }
 }
 
 struct DirectoryAuthority {
